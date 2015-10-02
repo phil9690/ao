d67d00ace78c28ff442a7e0f15b6d7eb2e31e67b
Phil <p.clauzel90@gmail.com>
2015-10-02 18:29:21 +0100

    Add sticky nav and smooth scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,29 @@ a = function() {
   Waves.displayEffect();
 }
 
+$(window).scroll(function () {
+  if ($(document).scrollTop() == 0) {
+    $('nav').removeClass('sticky');
+  } else {
+    $('nav').addClass('sticky');
+  }
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top - 60
+    }, 1000);
+    return false;
+  }
+  }
+  });
+});
+
 $(document).on("ready page:load", a);
 
 
